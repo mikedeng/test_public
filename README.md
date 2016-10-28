256807a560521751b56c676fb18af807b7a016b8
DengMing <dengm@udesk.cn>
2016-10-28 15:01:16 +0800

    quick fix

---
+++ README.md
@@ -0,0 +1,8 @@
+## README
+
+This project it used for self-testing.
+
+
+### test history
+
+* rest-client 301
--- test/controllers/rests_controller_test.rb
+++ test/controllers/rests_controller_test.rb
@@ -1,14 +1,11 @@
 require 'test_helper'
 
 class RestsControllerTest < ActionController::TestCase
-  test "should get index" do    
+  test "should post" do    
     rst = RestClient.get('http://localhost:3000/rests', {})
     if [301, 302, 307].include?(rst.code)
       rst.follow_redirection
-    end
-    binding.pry
-
-    assert_response :success
+    end    
   end
 
 end
